b70bb4484e91352569362c8392997e3d545f408f
Mr Fidal <154952367+mr-fidal@users.noreply.github.com>
2024-05-17 20:22:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='torspy',
-    version='1.0.1',
+    version='1.0.2',
     author='Fidal',
     author_email='mrfidal@proton.me',
-    description='Tor onion site scraping tool',
+    description='torspy is a robust Python package fortified with powerful algorithms, designed for seamless exploration of .onion sites via the Tor network. Its arsenal includes adept scraping of HTML from .onion URLs, precise text localization within the acquired content, and proficient storage of findings. Moreover, torspy boasts formidable subdomain scanning capabilities, enabling thorough reconnaissance across diverse subdomains. Additionally, it excels at detecting hidden directories, further enhancing its efficacy in navigating and extracting valuable information from the depths of the dark web.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/mr-fidal/torspy',
