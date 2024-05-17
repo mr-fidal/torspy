1ab088489d99722b2dd0e12d6be594bac8bcb7d7
Mr Fidal <154952367+mr-fidal@users.noreply.github.com>
2024-05-17 19:39:48 +0530

    Update scraper.py

--- torspy/scraper.py
+++ torspy/scraper.py
@@ -95,6 +95,7 @@ def scrape_onion_site(url, search_query=None, save_file=None, save_directory=Non
         else:
             print(html_content)
 
+# Update find_directories function
 def find_directories(url, dir_file, save_file=None, save_directory=None):
     check_tor_running()
     site_exists, message = check_onion_site(url)
@@ -121,7 +122,7 @@ def find_directories(url, dir_file, save_file=None, save_directory=None):
         test_url = os.path.join(url, directory)
         try:
             response = session.get(test_url, timeout=60)
-            if response.status_code == 200:
+            if response.status_code == 200 and ("index.html" in response.text or "index.php" in response.text):
                 found_directories.append(test_url)
                 print(f"Found: {test_url}")
         except requests.RequestException:
@@ -144,5 +145,56 @@ def find_directories(url, dir_file, save_file=None, save_directory=None):
             except IOError as e:
                 print(f"Error saving the file: {e}")
     else:
-        print("\nNo directories found.")
-    
+        print("\nNo directories found.")  
+
+def find_subdomains(url, sub_file, save_file=None, save_directory=None):
+    check_tor_running()
+
+    if not url.startswith("http://") and not url.startswith("https://"):
+        url = "http://" + url
+
+    base_url = url.split(".onion")[0] + ".onion"
+
+    session = requests.session()
+    session.proxies = {
+        'http': 'socks5h://127.0.0.1:9050',
+        'https': 'socks5h://127.0.0.1:9050'
+    }
+
+    try:
+        with open(sub_file, 'r') as file:
+            subdomains = file.readlines()
+    except IOError as e:
+        print(f"Error reading the file: {e}")
+        return
+
+    found_subdomains = []
+    for subdomain in subdomains:
+        subdomain = subdomain.strip()
+        test_url = f"http://{subdomain}.{base_url}"
+        try:
+            response = session.get(test_url, timeout=60)
+            if response.status_code == 200:
+                found_subdomains.append(test_url)
+                print(f"Found: {test_url}")
+        except requests.RequestException:
+            continue
+
+    if found_subdomains:
+        print("\nFound subdomains:")
+        for sub in found_subdomains:
+            print(sub)
+        
+        if save_file:
+            if save_directory:
+                save_path = os.path.join(save_directory, save_file)
+            else:
+                save_path = save_file
+            try:
+                with open(save_path, 'w', encoding='utf-8') as file:
+                    file.write('\n'.join(found_subdomains))
+                print(f"Found subdomains saved to {save_path}")
+            except IOError as e:
+                print(f"Error saving the file: {e}")
+    else:
+        print("\nNo subdomains found.")
