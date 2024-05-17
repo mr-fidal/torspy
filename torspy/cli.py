dafac04f397294dd145473ff0f1e497efeb6738c
Mr Fidal <154952367+mr-fidal@users.noreply.github.com>
2024-05-17 19:41:22 +0530

    Update cli.py

--- torspy/cli.py
+++ torspy/cli.py
@@ -2,12 +2,12 @@
 # copyright ©️ 2024 author Fidal
 
 import argparse
-from .scraper import scrape_onion_site, find_directories
+from .scraper import scrape_onion_site, find_directories, find_subdomains
 
 def main():
     epilog_text = '''
-Copyright (c) 2024 Author: Fidal
-Report an Issue : https://github.com/mr-fidal/torspy/issues
+Copyright (©️) 2024 author: Fidal
+Issue: https://GitHub.com/mr-fidal/torspy
 '''
     parser = argparse.ArgumentParser(description='Scrape a .onion site.', epilog=epilog_text,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -15,11 +15,14 @@ Report an Issue : https://github.com/mr-fidal/torspy/issues
     parser.add_argument('--find', type=str, help='The text to search for within the site')
     parser.add_argument('-s', '--save', type=str, help='The file name to save the content')
     parser.add_argument('-d', '--directory', type=str, help='The directory to save the file')
-    parser.add_argument('--dir', type=str, help='File with list of directories to check')
+    parser.add_argument('--dir', type=str, help='File containing list of directories to check')
+    parser.add_argument('--sub', type=str, help='File containing list of subdomains to check')
     args = parser.parse_args()
-    
+
     if args.dir:
         find_directories(args.url, args.dir, args.save, args.directory)
+    elif args.sub:
+        find_subdomains(args.url, args.sub, args.save, args.directory)
     else:
         scrape_onion_site(args.url, args.find, args.save, args.directory)
 
