504f43c2c9ead46963fcc0b38adf4d03c24314ed
Mr Fidal <154952367+mr-fidal@users.noreply.github.com>
2024-05-17 09:46:07 +0530

    Update scraper.py

--- torspy/scraper.py
+++ torspy/scraper.py
@@ -12,10 +12,11 @@ logger = logging.getLogger(__name__)
 
 def setup_session(proxy=None, user_agent=None):
     session = requests.Session()
-    session.proxies = {
-        'http': proxy if proxy else 'socks5h://127.0.0.1:9050',
-        'https': proxy if proxy else 'socks5h://127.0.0.1:9050'
-    }
+    if proxy:
+        session.proxies = {
+            'http': proxy,
+            'https': proxy
+        }
     if user_agent:
         session.headers.update({'User-Agent': user_agent})
     return session
@@ -23,6 +24,7 @@ def setup_session(proxy=None, user_agent=None):
 def check_onion_site(url, session):
     if not (url.startswith("http://") or url.startswith("https://")):
         url = "http://" + url
+        logger.info(f"No scheme supplied. Assuming http:// for {url}")
 
     try:
         response = session.get(url, timeout=60)
@@ -37,12 +39,14 @@ def check_onion_site(url, session):
         logger.warning(f"Site returned status code: {response.status_code}")
         return False, f"Site returned status code: {response.status_code}"
 
-def scrape_onion_site(url, search_query=None, save_file=None, save_directory=None, proxy=None, user_agent=None):
+def scrape_onion_site(url, search_query=None, save_file=None, save_directory=None, proxy=None, user_agent=None, directories_file=None):
     session = setup_session(proxy, user_agent)
     site_exists, message = check_onion_site(url, session)
     if not site_exists:
         logger.info(message)
         return
+
+    found_urls = []
 
     try:
         response = session.get(url, timeout=60)
@@ -52,6 +56,19 @@ def scrape_onion_site(url, search_query=None, save_file=None, save_directory=Non
         return
 
     soup = BeautifulSoup(response.text, 'html.parser')
+
+    if directories_file:
+        with open(directories_file, 'r') as file:
+            directories = [line.strip() for line in file.readlines()]
+
+        for directory in directories:
+            directory_url = f"{url}/{directory}"
+            directory_exists, _ = check_onion_site(directory_url, session)
+            if directory_exists:
+                logger.info(f"Directory found: {directory_url}")
+                found_urls.append(directory_url)
+            else:
+                logger.info(f"Directory not found: {directory_url}")
 
     if search_query:
         results = soup.find_all(string=lambda text: search_query.lower() in text.lower())
@@ -78,4 +95,7 @@ def scrape_onion_site(url, search_query=None, save_file=None, save_directory=Non
                 logger.error(f"Error saving the file: {e}")
         else:
             print(html_content)
-    
+
+    # Print found URLs
+    for found_url in found_urls:
+        print(f"Directory found: {found_url}")
