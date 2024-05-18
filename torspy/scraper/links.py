4153e9bd39cdd248453b102fec18a2ebf8dba458
Mr Fidal <154952367+mr-fidal@users.noreply.github.com>
2024-05-19 04:52:06 +0530

    Update links.py

--- torspy/scraper/links.py
+++ torspy/scraper/links.py
@@ -2,12 +2,13 @@
 # Copyright (©️) 2024 author: Fidal
 # Issue : https://github.com/mr-fidal/torspy
 
+import os
 import requests
 from bs4 import BeautifulSoup
 from .tor_check import check_tor_running
 from .html_extract import check_onion_site
 
-def extract_links(url):
+def extract_links(url, save_file=None, save_directory=None):
     check_tor_running()
     site_exists, message = check_onion_site(url)
     if not site_exists:
@@ -28,9 +29,24 @@ def extract_links(url):
         return
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    links = [a['href'] for a in soup.find_all('a', href=True)]
+    links = soup.find_all('a', href=True)
 
+    extracted_links = [link['href'] for link in links]
+    
     print("Extracted links:")
-    for link in links:
+    for link in extracted_links:
         print(link)
-  
+
+    if save_file:
+        if save_directory:
+            save_path = os.path.join(save_directory, save_file)
+        else:
+            save_path = save_file
+
+        try:
+            with open(save_path, 'w', encoding='utf-8') as file:
+                file.write('\n'.join(extracted_links))
+            print(f"Links saved to {save_path}")
+        except IOError as e:
+            print(f"Error saving the file: {e}")
+    
