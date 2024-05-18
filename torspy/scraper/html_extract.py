4ca032272893540db17aa6e2ccb4ff96735ff6ed
Mr Fidal <154952367+mr-fidal@users.noreply.github.com>
2024-05-19 00:40:15 +0530

    Update html_extract.py

--- torspy/scraper/html_extract.py
+++ torspy/scraper/html_extract.py
@@ -1,9 +1,9 @@
-# torspy/scraper/html_extract.py
+#!/usr/bin/python3
+# Copyright (©️) 2024 author: Fidal
+# Issue : https://github.com/mr-fidal/torspy
 
-import os
 import requests
 from bs4 import BeautifulSoup
-from .tor_check import check_tor_running
 
 def check_onion_site(url):
     if not url.startswith("http://") and not url.startswith("https://"):
@@ -26,8 +26,7 @@ def check_onion_site(url):
     else:
         return False, f"Site returned status code: {response.status_code}"
 
-def scrape_onion_site(url, search_query=None, save_file=None, save_directory=None):
-    check_tor_running()
+def extract_html(url):
     site_exists, message = check_onion_site(url)
     if not site_exists:
         print(message)
@@ -47,35 +46,6 @@ def scrape_onion_site(url, search_query=None, save_file=None, save_directory=Non
         return
 
     soup = BeautifulSoup(response.text, 'html.parser')
-
-    if search_query:
-        results = soup.find_all(string=lambda text: search_query.lower() in text.lower())
-        result_content = '\n'.join([str(result.parent) for result in results])
-        if save_file:
-            if save_directory:
-                save_path = os.path.join(save_directory, save_file)
-            else:
-                save_path = save_file
-            try:
-                with open(save_path, 'w', encoding='utf-8') as file:
-                    file.write(result_content)
-                print(f"Content matching '{search_query}' saved to {save_path}")
-            except IOError as e:
-                print(f"Error saving the file: {e}")
-        else:
-            print(result_content)
-    else:
-        html_content = soup.prettify()
-        if save_file:
-            if save_directory:
-                save_path = os.path.join(save_directory, save_file)
-            else:
-                save_path = save_file
-            try:
-                with open(save_path, 'w', encoding='utf-8') as file:
-                    file.write(html_content)
-                print(f"HTML content saved to {save_path}")
-            except IOError as e:
-                print(f"Error saving the file: {e}")
-        else:
-            print(html_content)
+    html_content = soup.prettify()
+    print(html_content)
+    
