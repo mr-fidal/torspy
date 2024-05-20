dbdddb0be0c09325d7cc158cf776363e17f8f605
Mr Fidal <154952367+mr-fidal@users.noreply.github.com>
2024-05-20 13:48:58 +0530

    Update cli.py

--- torspy/cli.py
+++ torspy/cli.py
@@ -3,25 +3,25 @@
 # Issue : https://github.com/mr-fidal/torspy
 
 import argparse
-from .scraper.scrape import scrape_onion_site
+from .scraper.scrape import scrape_site
 from .scraper.directories import find_directories
 from .scraper.subdomains import find_subdomains
 from .scraper.html_extract import extract_html
 from .scraper.analyze import analyze_content
 from .scraper.download import download_content
 from .scraper.links import find_links
-from .scraper.services import get_service_info 
+from .scraper.services import get_service_info
 
 def main():
     epilog_text = '''
-\ntorspy is a robust Python package fortified with powerful algorithms, designed for seamless exploration of .onion sites via the Tor network. Its arsenal includes adept scraping of HTML from .onion URLs, precise text localization within the acquired content, and proficient storage of findings. Moreover, torspy boasts formidable subdomain scanning capabilities, enabling thorough reconnaissance across diverse subdomains. Additionally, it excels at detecting hidden directories, further enhancing its efficacy in navigating and extracting valuable information from the depths of the dark web.\n\n
+torspy is a robust Python package fortified with powerful algorithms, designed for seamless exploration of websites including .onion sites via the Tor network. Its arsenal includes adept scraping of HTML from URLs, precise text localization within the acquired content, and proficient storage of findings. Moreover, torspy boasts formidable subdomain scanning capabilities, enabling thorough reconnaissance across diverse subdomains. Additionally, it excels at detecting hidden directories, further enhancing its efficacy in navigating and extracting valuable information from the depths of the web.\n\n
 Copyright (c) 2024 author: Fidal
 Report an Issue : https://github.com/mr-fidal/torspy/issues
     '''
 
-    parser = argparse.ArgumentParser(description='Scrape a .onion site.', epilog=epilog_text,
+    parser = argparse.ArgumentParser(description='Scrape a website.', epilog=epilog_text,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('url', type=str, help='The .onion site URL to scrape')
+    parser.add_argument('url', type=str, help='The site URL to scrape')
     parser.add_argument('--find', type=str, help='The text to search for within the site')
     parser.add_argument('-s', '--save', type=str, help='The file name to save the content')
     parser.add_argument('-d', '--directory', type=str, help='The directory to save the file')
@@ -47,7 +47,7 @@ Report an Issue : https://github.com/mr-fidal/torspy/issues
     elif args.service:
         get_service_info(args.url, args.save, args.directory)
     else:
-        scrape_onion_site(args.url, args.find, args.save, args.directory)
+        scrape_site(args.url, args.find, args.save, args.directory)
 
 if __name__ == "__main__":
     main()
